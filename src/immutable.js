03a7a2a65b133274dc9430759cdb6ae62b33af19
Gustavo Konrad <g.konrad@outlook.com>
2020-05-16 23:54:44 -0300

    formatting

--- src/immutable.js
+++ src/immutable.js
@@ -5,29 +5,29 @@ import diff from './differ.js'
 // update the store, creating subtrees if needed
 // actions can destructure the state to change only what they need
 export default function immutable(state = {}, middleware) {
-	const { subscribe, update } = writable(state)
-	
-	const history = []
-	
-	// All changes are triggered through dispatch
-	// dispatch is: (action, ...args) => action(state, ...args)
-	// actions are: (state, ...args) => (state)
+  const { subscribe, update } = writable(state)
+
+  const history = []
+
+  // All changes are triggered through dispatch
+  // dispatch is: (action, ...args) => action(state, ...args)
+  // actions are: (state, ...args) => (state)
   // middlewares are: (state) => (next, args) => ... => (state)
   const dispatch = (action, ...args) => {
-		update(state => {
-			const old = state
+    update(state => {
+      const old = state
 
-			if (typeof middleware === "function") {
+      if (typeof middleware === "function") {
         state = middleware(state, action, args)
-				history.push({ new: state, old, diff: diff(state, old), action, middleware })
-				return state
+        history.push({ new: state, old, diff: diff(state, old), action, middleware })
+        return state
       }
 
-			state = action(state, ...args)
-			history.push({ new: state, old, diff: diff(state, old), action })
-			return state
-		})
-	}
+      state = action(state, ...args)
+      history.push({ new: state, old, diff: diff(state, old), action })
+      return state
+    })
+  }
 
-	return [{ subscribe }, dispatch, history, middleware]
+  return [{ subscribe }, dispatch, history, middleware]
 }
